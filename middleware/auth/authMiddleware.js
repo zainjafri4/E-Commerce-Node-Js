7442d47533fedd5bd9ac9458996b03a46d76972e
unknown <zainjafri4@gmail.com>
2024-06-14 23:04:51 +0500

    fix: authMiddleware fixed

--- middleware/auth/authMiddleware.js
+++ middleware/auth/authMiddleware.js
@@ -1,13 +1,16 @@
 const jwt = require("jsonwebtoken");
 const User = require("../../models/userModels/auth.js");
 
+const jwt = require("jsonwebtoken");
+const User = require("./models/User"); // Adjust the path as necessary
+
 const authMiddleware = async (req, res, next) => {
   // Get token from Authorization header
   const authHeader = req.headers["authorization"];
   if (!authHeader) {
     return res.status(401).json({
-      Success: false,
-      message: "Token Not Found invalid",
+      success: false,
+      message: "Token Not Found",
     });
   }
 
@@ -15,19 +18,37 @@ const authMiddleware = async (req, res, next) => {
   const token = authHeader.split(" ")[1];
   if (!token) {
     return res.status(401).json({
-      Success: false,
+      success: false,
       message: "Token Not Valid, Authorization denied",
     });
   }
 
   // Verify token
   try {
-    const decoded = jwt.verify(token, "secret");
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
     let user = await User.findById(decoded.id).select("-password");
+
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: "User not found",
+      });
+    }
+
     req.user = user;
     next();
   } catch (err) {
     console.error(err.message);
+
+    // Check if the error is a JWT error
+    if (err.name === "JsonWebTokenError") {
+      return res.status(401).json({
+        success: false,
+        message: "Unauthorized: Invalid token",
+      });
+    }
+
+    // For other errors, return a generic internal server error
     return res.status(500).json({
       success: false,
       message: "Internal Server Error",
