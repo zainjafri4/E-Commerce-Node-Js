970a768dbc2c938c76b7222a5e3cfd4598a1c383
unknown <zainjafri4@gmail.com>
2024-06-15 18:52:30 +0500

    fix: userId added in product

--- models/adminModels/productModels.js
+++ models/adminModels/productModels.js
@@ -2,6 +2,11 @@ const mongoose = require("mongoose");
 
 const productSchema = new mongoose.Schema(
   {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
     title: {
       type: String,
       required: true,
